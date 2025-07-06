6597a454250c93e9895720c809155d65793bae13
Khushi Ghumre <khushighumre@gmail.com>
2025-07-06 13:03:57 +0530

    DOne with Homepage nd loginpage

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,53 +1,67 @@
-// // import React from 'react';
 
-// // const HomePage = () => {
-// //   return 
-    
-// //       <h1>Home Page</h1>;
-    
-  
-// // };
+// import React, { useState } from 'react';
+// import ChatContainer from '../components/ChatContainer';
+// import RightSidebar from '../components/RightSidebar';
+// import Sidebar from '../components/Sidebar';
 
-// // export default HomePage;
-// import React from 'react';
 
 // const HomePage = () => {
-//   return (
-//   <div className='border w-full h-screen sm:px-[15%] sm:py-[5%]'>
 
-//   </div>
-  
-// );
+//     const [selectedUser, setSelectedUser] = useState(false)
+
+//   return (
+//     <div className="border w-full h-screen sm:px-[15%] sm:py-[5%]">
+//       <div className={`backdrop-blur-xl border-2 border-gray-600 rounded-2xl
+//       overflow-hidden h-[100%] grid grid-cols-1 relative ${
+//     selectedUser
+//       ? 'md:grid-cols-[1fr_1.5fr_1fr] xl:grid-cols-[1fr_2fr_1fr]'
+//       : 'md:grid-cols-2'
+//   }`}>
+//         <Sidebar />
+//         <ChatContainer />
+//         <RightSidebar />
+        
+//       </div>
+
+//     </div>
+    
+//   );
 // };
 
 // export default HomePage;
+
 
 import React, { useState } from 'react';
 import ChatContainer from '../components/ChatContainer';
 import RightSidebar from '../components/RightSidebar';
 import Sidebar from '../components/Sidebar';
 
-
 const HomePage = () => {
-
-    const [selectedUser, setSelectedUser] = useState(false)
+  const [selectedUser, setSelectedUser] = useState(null);
 
   return (
     <div className="border w-full h-screen sm:px-[15%] sm:py-[5%]">
       <div className={`backdrop-blur-xl border-2 border-gray-600 rounded-2xl
-      overflow-hidden h-[100%] grid grid-cols-1 relative ${
-    selectedUser
-      ? 'md:grid-cols-[1fr_1.5fr_1fr] xl:grid-cols-[1fr_2fr_1fr]'
-      : 'md:grid-cols-2'
-  }`}>
-        <Sidebar />
-        <ChatContainer />
-        <RightSidebar />
+        overflow-hidden h-full grid grid-cols-1 relative ${
+          selectedUser
+            ? 'md:grid-cols-[1fr_1.5fr_1fr] xl:grid-cols-[1fr_2fr_1fr]'
+            : 'md:grid-cols-2'
+        }`}>
         
-      </div>
+        <Sidebar
+          selectedUser={selectedUser}
+          setSelectedUser={setSelectedUser}
+        />
 
+        <ChatContainer
+          selectedUser={selectedUser}
+          setSelectedUser={setSelectedUser}
+        />
+
+        <RightSidebar selectedUser={selectedUser} />
+
+      </div>
     </div>
-    
   );
 };
 
